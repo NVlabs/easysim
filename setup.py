239a283bd66dc9fe9f45d546050313ba873f755f
Yu-Wei Chao <ychao@nvidia.com>
2022-02-07 20:38:13 -0800

    Import .gitignore/MANIFEST.in/README.md/pyproject.toml/setup.cfg/setup.py/src/tests from SRL Python Project

---
+++ setup.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
+# property and proprietary rights in and to this material, related
+# documentation and any modifications thereto. Any use, reproduction,
+# disclosure or distribution of this material and related documentation
+# without an express license agreement from NVIDIA CORPORATION or
+# its affiliates is strictly prohibited.
+"""__Python_Project_Template__ package setuptools."""
+
+# NOTE (roflaherty): This file is still needed to allow the package to be
+# installed in editable mode.
+#
+# References:
+# * https://setuptools.pypa.io/en/latest/setuptools.html#setup-cfg-only-projects
+
+# Third Party
+import setuptools
+
+setuptools.setup()
